a403cf2eafcd167bc569f2e55851286de17bc0e9
Brian Kereszturi <briankereszturi@gmail.com>
2017-05-18 14:08:28 -0700

    Add migrate task.

---
+++ lib/expostgres_helpers/tasks.ex
@@ -0,0 +1,6 @@
+defmodule ExpostgresHelpers.Tasks do
+  def migrate(repo, app) do
+    path = Application.app_dir(app, "priv/repo/migrations")
+    Ecto.Migrator.run(repo, path, :up, all: true)
+  end 
+end
